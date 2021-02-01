90195892bf445284dc41a9bcb2a5052e8d7b1467
radudin11 <78203390+radudin11@users.noreply.github.com>
2021-02-01 11:42:47 +0200

    Update CardSign.cs
    Co-authored-by: Dan Dinicescu <rodede@gmail.com>

--- CardSign.cs
+++ CardSign.cs
@@ -4,6 +4,6 @@ using System.Text;
 
 namespace cardGameSix
 {
-    public enum Sign : int { HEARTS = 1, DIAMONDS = 2, SPADES = 3, CLUBS = 4 };
+    public enum CardSign : int { HEARTS = 1, DIAMONDS = 2, SPADES = 3, CLUBS = 4 };
     // Creating a class for each sign of the playing card.
 }
